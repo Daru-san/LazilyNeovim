28c4f53249f76ae767fbd0e1ccb800201af3ddcd
Daru-san <mica.tui@proton.me>
2023-11-24 14:17:59 +0200

    feat: add transparency keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,5 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local Util = require("lazyvim.util")
+
+local map = vim.keymap.set
+
+map("n", "<leader>T","<cmd>TransparentToggle<cr>", {desc = "Toggle transparency"})
