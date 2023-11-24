8682db628d38c7fd172c97ed51b4749588815503
Daru-san <mica.tui@proton.me>
2023-11-24 14:19:09 +0200

    feat: update plugins

---
+++ lua/plugins/everforest.lua
@@ -0,0 +1,12 @@
+return {
+  -- everforest-nvim
+  { "neanias/everforest-nvim" },
+
+  -- Configure LazyVim to load everforest-nvim
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
