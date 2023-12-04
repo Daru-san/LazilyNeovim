9b4c53c61e2983cd7af01e46e1e4cd7a4bea08d1
Daru-san <mica.tui@proton.me>
2023-12-04 18:22:03 +0200

    feat: update plugins

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,13 @@
+-- lazy.nvim
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+    -- u
+  },
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+}
